8b476f8e75983c2e74e18a80d11d11c03a45c2ab
Joy Kim <jojo080889@gmail.com>
2013-11-18 21:34:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atelier::Application.config.secret_token = 'f1d034eb893420a3dddd2315702437d189a3559f5abaa56f6c7ad1a5ad2f0b7ee1e62d03e2293b34f29ebcb8ce75ce3bf9aa629e943f2cf8b081424a241df3ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Atelier::Application.config.session_store :cookie_store, key: '_atelier_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Atelier::Application.config.session_store :active_record_store
