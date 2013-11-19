130d384eb5f1dadbd238b2ad12c94a6803db9b81
Joy Kim <jojo080889@gmail.com>
2013-11-19 00:43:58 -0800

    config change for heroku

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Atelier
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # For Heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
