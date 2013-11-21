de2d6bd0c8b201d8032961e2a522ac8c9b983e96
Joy Kim <jojo080889@gmail.com>
2013-11-20 23:28:09 -0800

    adds http:// to project url if not present

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,13 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   attr_accessible :title, :url, :user_id
+  before_validation :smart_add_url_protocol
+
+  protected
+
+  def smart_add_url_protocol
+    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+      self.url = "http://#{self.url}"
+    end
+  end
 end
