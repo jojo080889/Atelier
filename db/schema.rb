fd75c26104b95f29644e070582483243f6bcfde9
Joy Kim <jojo080889@gmail.com>
2013-11-21 15:38:43 -0800

    migrate db for new critique model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131119073852) do
+ActiveRecord::Schema.define(:version => 20131121233559) do
+
+  create_table "critiques", :force => true do |t|
+    t.integer  "mentor_id"
+    t.integer  "project_id"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "title"
