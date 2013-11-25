945e9451304f8b5adf3dddcfaa53c1d506bb6ede
Joy Kim <jojo080889@gmail.com>
2013-11-25 11:35:55 -0800

    restyle critiques

--- app/helpers/critiques_helper.rb
+++ app/helpers/critiques_helper.rb
@@ -5,13 +5,13 @@ module CritiquesHelper
     if current_user.voted_up_on? critique
       link_to project_critique_unlike_path(critique.project, critique), class: "unlike-link", method: :post, remote: true do
         html += fa_icon "thumbs-up"
-        html += " Liked"
+        html += " Marked as helpful"
         html.html_safe
       end
     else
       link_to project_critique_like_path(critique.project, critique), class: "like-link", method: :post, remote: true do
         html += fa_icon "thumbs-up" 
-        html += " Like this critique"  
+        html += " Mark this critique as helpful"  
         html.html_safe
       end
     end
