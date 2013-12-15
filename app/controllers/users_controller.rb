b4594f6068c36b87bfaa5396f1bb2870cbdc9d36
Joy Kim <jojo080889@gmail.com>
2013-12-14 22:42:27 -0800

    My Projects only shows up when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+  
   def show
     @user = User.find_by_username(params[:id])
   end
