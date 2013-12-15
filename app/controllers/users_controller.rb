42f2289e92c75ea13aa5feb603f9ede9d3582bf3
Joy Kim <jojo080889@gmail.com>
2013-12-14 22:56:02 -0800

    message for showing entries don't have content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  load_and_authorize_resource :find_by => :username
   
   def show
     @user = User.find_by_username(params[:id])
