9eab1848efe9a0bc7322e2c5c0e3c5ef6fc1aafe
Joy Kim <jojo080889@gmail.com>
2013-12-15 09:17:35 -0800

    change projects description so it's longer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131212170917) do
+ActiveRecord::Schema.define(:version => 20131215171605) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(:version => 20131212170917) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
