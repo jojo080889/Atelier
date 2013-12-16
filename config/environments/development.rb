508ee2e2568078c57f96ba28f3b9c46a497cab24
Joy Kim <jojo080889@gmail.com>
2013-12-16 12:52:39 -0800

    proper paperclip config in development, images can be clicked on to be make bigger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ Atelier::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['ATELIER_S3_BUCKET'],
-      :access_key_id => ENV['ATELIER_AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['ATELIER_AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
