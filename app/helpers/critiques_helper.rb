99c428d4fd4cadacde5fde7c004c0d3d1af47507
Joy Kim <jojo080889@gmail.com>
2013-12-29 19:25:46 -0800

    conditional textbox guidance for critique

--- app/helpers/critiques_helper.rb
+++ app/helpers/critiques_helper.rb
@@ -16,4 +16,33 @@ module CritiquesHelper
       end
     end
   end
+
+  def critique_hints(user)
+    if user.skill_level == "Beginner"
+      html = "<ul>"
+      html += "<li>"
+      html += "Be respectful, encouraging, and specific with your comments."
+      html += "</li>"
+      html += "<li>"
+      html += "Pointing to resources that have helped you in the past is a good way to give advice."
+      html += "</li>"
+      html += "</ul>"
+    else
+      html = "<ul>"
+      html += "<li>"
+      html += "Be respectful, encouraging, and specific with your comments."
+      html += "</li>"
+      html += "<li>"
+      html += "Comment on your mentee\'s technical skill. What are some great ways they used the tools and materials at their disposal? What are some ways they could improve?"
+      html += "</li>"
+      html += "<li>"
+      html += "As a viewer, what is your reaction to the piece? What do you notice about the piece's content, and how do you feel about it?"
+      html += "</li>"
+      html += "<li>"
+      html += "If their work was particularly well-done for their skill level, say so through your overall rating of the project."
+      html += "</li>"
+      html += "</ul>"
+    end
+    html.html_safe
+  end
 end
