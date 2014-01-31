27dfef87ffc1f472f91c346943051fd49fd8cfc8
Joy Kim <jojo080889@gmail.com>
2014-01-31 11:19:33 -0800

    make postmark domain url an environment setting

--- config/initializers/mail_settings.rb
+++ config/initializers/mail_settings.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['POSTMARK_SMTP_SERVER'],
   :user_name      => ENV['POSTMARK_API_KEY'],
   :password       => ENV['POSTMARK_API_KEY'],
-  :domain         => 'atelier-stanford.heroku.com',
+  :domain         => ENV['POSTMARK_DOMAIN'],
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
