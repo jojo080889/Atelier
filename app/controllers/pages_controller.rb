747cc6457742860796dc6ef4d757e425ec70d9b7
Joy Kim <jojo080889@gmail.com>
2014-02-05 21:59:24 -0800

    badges on dashboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!, :only => :dashboard
   
   def dashboard
+    @available_badges = current_user.skill_level.badges
+    @earned_badges = current_user.badges
   end
 
   def about
