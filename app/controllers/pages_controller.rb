e6f5802132f935a371201b7be683894cc80ac6b9
Joy Kim <jojo080889@gmail.com>
2014-02-06 00:04:22 -0800

    notification section on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   def dashboard
     @available_badges = current_user.skill_level.badges
     @earned_badges = current_user.badges
+
+    @tutorial_not_done = current_user.skill_level.lowest? && !current_user.tutorial_done?
+    @too_many_projects = current_user.critiques.count < current_user.projects.count
   end
 
   def about
