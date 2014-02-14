58c76d5dfe8e485b213d4485504ec49e09913ee7
Joy Kim <jojo080889@gmail.com>
2014-02-14 13:10:14 -0800

    prompts notification on dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  def current_prompt
+    if Time.now() < Time.new(2014, 2, 21, 23, 59, 0)
+      {:text => "Draw something that starts with the first letter of your name.", 
+       :time => Time.new(2014, 2, 21, 23, 59, 0)}
+    elsif Time.now() < Time.new(2014, 2, 28, 23, 59, 0)
+      {:text => "Illustrate a line from a song.",
+       :time => Time.new(2014, 2, 28, 23, 59, 0)}
+    elsif Time.now() < Time.new(2014, 3, 7, 23, 59, 0)
+      {:text => "Draw somewhere new. Go somewhere new to you and draw what you find.",
+       :time => Time.new(2014, 3, 7, 23, 59, 0)}
+    elsif Time.now() < Time.new(2014, 3, 14, 23, 59, 0)
+      {:text => "Draw something inspired by the word <i>bridge</i>.",
+       :time => Time.new(2014, 3, 14, 23, 59, 0)}
+    else
+      nil
+    end
+  end
 end
